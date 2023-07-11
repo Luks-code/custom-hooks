994a74ea3923c614813103ed83a49882beb22faf
Luks-code <lucas123gamerpro@gmail.com>
2023-07-11 18:21:54 -0300

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,38 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
+export const useTodo = () => {
+  const [todos, dispatchTodo] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const onNewTodo = (todo) => {
+    dispatchTodo({
+      type: '[ADD] Todo',
+      payload: todo,
+    });
+  };
+
+  const onDeleteTodo = (id) => {
+    dispatchTodo({ type: '[DELETE] Todo', payload: id });
+  };
+
+  const onToggleTodo = (id) => {
+    dispatchTodo({ type: '[TOGGLE] Todo', payload: id });
+  };
+
+  return {
+    todos,
+    onNewTodo,
+    onDeleteTodo,
+    onToggleTodo,
+    allTodosCount: todos.length,
+    pendingTodosCount: todos.filter((todo) => !todo.done).length,
+  };
+};
